7da31aba73b8caf52fdb786a74981f66a894bf19
Saveliy <miheev.saveliy.2002@gmail.com>
2023-11-13 23:55:50 +0800

    Remove all file

--- Luminex-Core/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import navbar from "@/components/navbar.vue";
-import calendar from "@/components/calendar.vue"
-
-
-export default [navbar,calendar];
--- Luminex-Core/src/pages/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div><h1>15.09.2023</h1></div>
-</template>
-
-<script>
-export default {};
-</script>
-<style></style>
